de0c2f0c6ce518a27abdfeed1e306fa88d795023
najlahamza <najla.hamza11@gmail.com>
2022-12-09 18:19:07 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://my-prod-url'
-  };
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDTyCRUaE_vi6HyGyAZs7AunzJlh9BNQsQ",
+    authDomain: "plant-1f218.firebaseapp.com",
+    projectId: "plant-1f218",
+    storageBucket: "plant-1f218.appspot.com",
+    messagingSenderId: "270129739978",
+    appId: "1:270129739978:web:6818c7417a670fffee9c33",
+    databaseURL: "https://plant-1f218-default-rtdb.europe-west1.firebasedatabase.app/"
+   // measurementId: "your-measurement-id"
+    }
+};
